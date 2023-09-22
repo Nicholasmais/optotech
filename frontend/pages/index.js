2c6ce8902737637b77389ecf3f2f8eb3323b5500
Nícholas Eras <48137930+Nicholasmais@users.noreply.github.com>
2023-09-22 15:32:11 -0300

    initial snellen and change button

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,13 +1,77 @@
 import styles from '../styles/Home.module.scss';
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [activeRow, setActiveRow] = useState(0);
+
+  const snellen_letters = [
+    {
+      letters: ["E"],
+      size:88
+    },
+    {
+      letters: ["F", "P"],
+      size:68
+    },
+    {
+      letters: ["T", "O", "Z"],
+      size:48
+    },
+    {
+      letters: ["L", "P", "E", "D"],
+      size:28
+    },
+    {
+      letters: ["P", "E", "C", "F", "D"],
+      size:12
+    }
+  ];
+
+  const changeActiveRow = (increment) => {
+    (increment < 0 && activeRow === 0) || (increment > 0 && activeRow === snellen_letters.length - 1) ?
+      null :  setActiveRow(activeRow + increment);
+  }
+  console.log(activeRow);
+
   return (
     <>
       <div className={styles.switch}>
-        Snellen / Personalize
+        <div className={styles.chart}>
+          <span>
+            Snellen
+          </span>
+        </div>
+        <div className={styles.personalize}>
+          <span>
+            Personalize
+          </span>
+        </div>        
       </div>
       <div className={styles.snellen}>
-        AEIOU
+        <div className={styles.row_to_read} style={{fontSize:`${snellen_letters[activeRow].size}px`}}>
+          {snellen_letters[activeRow].letters}
+        </div>
+        <div className={styles.all_letters}>
+          {
+            snellen_letters.map((row, index) => (
+              <div key={index} style={{fontSize:`${row.size}px`}}>
+                {row.letters}
+              </div>
+            )
+          )}  
+        </div>
+        <div className={styles.change_active_row}>
+          <button onClick={() => {
+              changeActiveRow(-1);            
+            }}>
+            Anterior
+          </button>
+          <button onClick={() => {
+              changeActiveRow(1);
+            }}>
+            Próximo
+          </button>
+        </div>
       </div>
     </>
   );
